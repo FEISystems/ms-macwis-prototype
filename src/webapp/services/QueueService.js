f6014dbb9af2ce082be24b6012fa64d1f549f219
Terry Boswell <trboswell74@comcast.net>
2016-10-04 14:29:02 -0400

    Bound the search and home page together Integrate the home page search with provider search #44

---
+++ src/webapp/services/QueueService.js
@@ -0,0 +1,24 @@
+﻿(function() {
+
+    var queueService = function($rootScope, $log) {
+
+        var messages = {};
+
+        var setMsg = function(name, data)
+        {
+            messages[name] = data;
+        }
+
+        var getMsg = function(name)
+        {
+            return messages[name];
+        }
+        return {
+            setMsg: setMsg,
+            getMsg: getMsg
+        };
+    };
+
+    var module = angular.module("macwisWebApp");
+    module.factory("queueService", queueService);
+}());
