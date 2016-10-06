f6ef3681e34663fc4942d59c9d1932904503d936
Terry Boswell <terry.boswell@fei.local>
2016-10-06 10:27:34 -0400

    Advanced Search Button on the Home Page #92 On the home page, can we make it clearer that the website is only for the state of MS? #75 Home page changes #68

--- src/webapp/views/shared/components/navbar-header/navbar-header.component.js
+++ src/webapp/views/shared/components/navbar-header/navbar-header.component.js
@@ -26,16 +26,7 @@
     //if (model.role == 'isasadmin' || model.role == 'provideradmin') {
 
         $scope.menuItems = [
-            {
-                'name': 'Home',
-                'link': ['Home'],
-                'path': 'home'
-            },
-            {
-                'name': 'Find Providers',
-                'link': ['ProviderSearch'],
-                'path': 'providers'
-            }
+            
         ];
 
 
