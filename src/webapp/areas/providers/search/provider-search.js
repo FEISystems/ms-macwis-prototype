f609c81a95fa564df25356a0298ee8850be3e6f0
BhavaniReddy8 <bhavani.reddy@feisystems.com>
2016-10-05 21:58:07 -0400

    ng-click added for Checkboxes

--- src/webapp/areas/providers/search/provider-search.js
+++ src/webapp/areas/providers/search/provider-search.js
@@ -12,14 +12,12 @@
         var model = this;
         model.title = "Provider Search";
         model.Criteria = {};
-        model.sortData = { up: true };
+        model.sortData = {up: true};
         model.provider = {};
         model.filteredProviders = [];
         // init end
 
         model.showProviderDetails = false;
-
-
 
 
         model.Cities = dataService.getCities();
@@ -39,63 +37,74 @@
         model.clickOk = function (provider) {
             model.showProviderDetails = false;
         };
-        model.search = function() {
+        model.search = function () {
             model.filteredProviders = [];
             var tempProviders = [];
-            model.sortData = { up: false, sortField: "QualityRating" };
+            model.sortData = {up: false, sortField: "QualityRating"};
             if (model.Criteria.address && model.Criteria.distince) {
-                googleMapService.getCoordinateByAddress(model.Criteria.address, function(coordinate) {
+                googleMapService.getCoordinateByAddress(model.Criteria.address, function (coordinate) {
                     tempProviders = providerService.getProvidersByDistince(coordinate.lat, coordinate.lng, model.Criteria.distince);
                     model.searchByCriteria(tempProviders);
-                }, function(error) {
+                }, function (error) {
                     alert(error);
                 });
             } else {
                 tempProviders = model.AllProviders;
                 model.searchByCriteria(tempProviders);
+            }
+        };
+
+            model.selected = [];
+            model.selectProviderToPrint = function ($event) {
+                var printpage = $event.currentTarget;
+                debugger;
+                if ($(printpage).is(":checked")) {
+                    model.selected.push($(printpage).attr('id'));
+                    console.log(model.selected);
+                } else {
+
+                }
             };
-            model.printprovider=function () {
 
-                var selected = [];
-                var htmlcode='';
+            // $('div#checkboxes input[type=checkbox]').each(function() {
+            //
+            // });
+               // $.each(selected,function (index,value) {
+                   // var TerValue=value.split(',')[16].split(':')[1].replace(/\"/g, "")=="true"?"Yes":"No";
+                   // htmlcode =htmlcode+
 
-                $('div#checkboxes input[type=checkbox]').each(function() {
-                    if ($(this).is(":checked")) {
-                        selected.push($(this).attr('id'));
-                    }
+            model.printprovider = function () {
+                var htmlcode = '';
+                debugger;
+                if(model.selected.length > 0) {
+                $.each(model.selected, function (index, value) {
+                    var TerValue = value.split(',')[16].split(':')[1].replace(/\"/g, "") == "true" ? "Yes" : "No";
+                    htmlcode = htmlcode + "<table><tr><div class='modal-body pad-no'><div class='row'> <div class='col-xs-offset-1'> <div class='col-md-12'><p class='ng-binding'><strong>Provider Name: </strong> " + value.split(',')[1].split(':')[1].replace(/\"/g, "") + "</p></div>" +
+                        "<div class='col-xs-4'><p class='ng-binding'><strong>Provider Type: </strong>  " + value.split(',')[4].split(':')[1].replace(/\"/g, "") + "</p> </div>" +
+                        "<div class='col-xs-4'><p class='ng-binding'><strong>Phone#: </strong>  " + value.split(',')[10].split(':')[1].replace(/\"/g, "") + "</p><p class='ng-binding'> <strong>City: </strong>" + value.split(',')[6].split(':')[1].replace(/\"/g, "") + " </p><p class='ng-binding'><strong>Quality Star Rating: </strong>   Excellent  </p>  " +
+                        "<p class='ng-binding'><strong>License Type: </strong>   " + value.split(',')[2].split(':')[1].replace(/\"/g, "") + " </p>" +
+                        "<p ng-show='provider.CanTakeChildrenWithBehavioralProblems===true' class='ng-hide'><strong>Accepts subsidized child care: </strong>" + TerValue + "</p>" +
+                        "<p ng-show='provider.CanTakeChildrenWithBehavioralProblems===false' class=''></p>" +
+                        "<p ng-show='provider.CanTakeChildrenWithBehavioralProblems!==false&amp;&amp; provider.CanTakeChildrenWithBehavioralProblems!==true' class='ng-binding ng-hide'></p>  </div>" +
+                        " <div class='col-xs-4'><p class='ng-binding'><strong>County: </strong>  " + value.split(',')[9].split(':')[1].replace(/\"/g, "") + "</p>" +
+                        "<p class='ng-binding'><strong>Zip Code: </strong> " + value.split(',')[7].split(':')[1].replace(/\"/g, "") + "</p>" +
+                        "    <p class='ng-binding'><strong>Provider Capacity: </strong> " + value.split(',')[5].split(':')[1].replace(/\"/g, "") + "</p>" +
+                        "<p class='ng-binding'><strong>Age Range: </strong> " + value.split(',')[13].split(':')[1].replace(/\"/g, "") + " to " + value.split(',')[14].split(':')[1].replace(/\"/g, "") + "  </p>" +
+                        "<p class='ng-binding'><strong>Gender: </strong> " + value.split(',')[15].split(':')[1].replace(/\"/g, "") + " </p></div></div></div></div></tr></table><hr/>";
+
                 });
-
-
-                $.each(selected,function (index,value) {
-                    var TerValue=value.split(',')[16].split(':')[1].replace(/\"/g, "")=="true"?"Yes":"No";
-                    htmlcode =htmlcode+ "<table><tr><div class='modal-body pad-no'><div class='row'> <div class='col-xs-offset-1'> <div class='col-md-12'><p class='ng-binding'><strong>Provider Name: </strong> "+value.split(',')[1].split(':')[1].replace(/\"/g, "")+"</p></div>"+
-                        "<div class='col-xs-4'><p class='ng-binding'><strong>Provider Type: </strong>  "+value.split(',')[4].split(':')[1].replace(/\"/g, "")+"</p> </div>"+
-                        "<div class='col-xs-4'><p class='ng-binding'><strong>Phone#: </strong>  "+value.split(',')[10].split(':')[1].replace(/\"/g, "")+"</p><p class='ng-binding'> <strong>City: </strong>"+value.split(',')[6].split(':')[1].replace(/\"/g, "")+" </p><p class='ng-binding'><strong>Quality Star Rating: </strong>   Excellent  </p>  "+
-                        "<p class='ng-binding'><strong>License Type: </strong>   "+value.split(',')[2].split(':')[1].replace(/\"/g, "")+" </p>"+
-                        "<p ng-show='provider.CanTakeChildrenWithBehavioralProblems===true' class='ng-hide'><strong>Accepts subsidized child care: </strong>"+ TerValue +"</p>" +
-                        "<p ng-show='provider.CanTakeChildrenWithBehavioralProblems===false' class=''></p>"+
-                        "<p ng-show='provider.CanTakeChildrenWithBehavioralProblems!==false&amp;&amp; provider.CanTakeChildrenWithBehavioralProblems!==true' class='ng-binding ng-hide'></p>  </div>"+
-                        " <div class='col-xs-4'><p class='ng-binding'><strong>County: </strong>  "+value.split(',')[9].split(':')[1].replace(/\"/g, "")+"</p>"+
-                        "<p class='ng-binding'><strong>Zip Code: </strong> "+value.split(',')[7].split(':')[1].replace(/\"/g, "")+"</p>"+
-                        "    <p class='ng-binding'><strong>Provider Capacity: </strong> "+value.split(',')[5].split(':')[1].replace(/\"/g, "")+"</p>"+
-                        "<p class='ng-binding'><strong>Age Range: </strong> "+value.split(',')[13].split(':')[1].replace(/\"/g, "")+" to "+value.split(',')[14].split(':')[1].replace(/\"/g, "")+"  </p>"+
-                        "<p class='ng-binding'><strong>Gender: </strong> "+value.split(',')[15].split(':')[1].replace(/\"/g, "")+" </p></div></div></div></div></tr></table><hr/>";
-
-                });
-                // htmlcode=htmlcode+'</table>';
-                if ($('div#checkboxes input[type=checkbox]').is(":checked")) {
+                // htmlcode=htmlcode+'</table>'; if ($('div#checkboxes input[type=checkbox]').is(":checked")) {
                     var popupWin = window.open('', '_blank', 'width=3000,height=3000');
                     popupWin.document.open();
                     popupWin.document.write('<html><head><link rel="stylesheet" type="text/css" href="scripts/vendor/bootstrap-3.3.7-dist/css/bootstrap.css" /></head><body onload="window.print()">' + htmlcode + '</body></html>');
                     popupWin.document.close();
                 }
-                else
-                {
+                else {
                     return false;
                 }
 
             };
-            model.setPage = function(num) {
+            model.setPage = function (num) {
                 if (num === -1) {
                     if (isNaN($("#GoPage").val()))
                         num = 0;
@@ -109,31 +118,31 @@
                 model.InitPages();
             };
 
-            model.prevPage = function() {
+            model.prevPage = function () {
                 if (model.currentPage > 0) {
                     model.currentPage--;
                 }
                 model.InitPages();
             };
-            model.nextPage = function() {
+            model.nextPage = function () {
                 if (model.currentPage < model.pages - 1) {
                     model.currentPage++;
                 }
                 model.InitPages();
             };
-            model.firstPage = function() {
+            model.firstPage = function () {
                 model.currentPage = 0;
                 model.InitPages();
             };
 
-            model.lastPage = function() {
+            model.lastPage = function () {
                 model.currentPage = model.pages - 1;
                 model.InitPages();
             };
 
-        };
 
-        model.searchByCriteria = function(tempProviders) {
+
+        model.searchByCriteria = function (tempProviders) {
 
             if (!model.Criteria || model.Criteria.ProviderName ||
                 model.Criteria.ProviderType || model.Criteria.City || model.Criteria.County ||
@@ -152,30 +161,31 @@
                         }
                     }
                     if (nameFound &&
-                    (!model.Criteria.ProviderType || (model.Criteria.ProviderType && model.Criteria.ProviderType === provider.ProviderType)) &&
-                    (!model.Criteria.City || (model.Criteria.City && model.Criteria.City === provider.PhysicalCity)) &&
-                    (!model.Criteria.County || (model.Criteria.County && model.Criteria.County === provider.CountyNumber)) &&
-                    (model.Criteria.Rate === undefined || model.Criteria.Rate === null || model.Criteria.Rate === provider.QualityRating) &&
-                    (!model.Criteria.Age || (model.Criteria.Age
+                        (!model.Criteria.ProviderType || (model.Criteria.ProviderType && model.Criteria.ProviderType === provider.ProviderType)) &&
+                        (!model.Criteria.City || (model.Criteria.City && model.Criteria.City === provider.PhysicalCity)) &&
+                        (!model.Criteria.County || (model.Criteria.County && model.Criteria.County === provider.CountyNumber)) &&
+                        (model.Criteria.Rate === undefined || model.Criteria.Rate === null || model.Criteria.Rate === provider.QualityRating) &&
+                        (!model.Criteria.Age || (model.Criteria.Age
                         && dataService.getAgeById(model.Criteria.Age) && dataService.getAgeById(model.Criteria.Age)[0] >= provider.MinAge
                         && dataService.getAgeById(model.Criteria.Age)[1] <= provider.MaxAge)) &&
-                    (!model.Criteria.Gender || (model.Criteria.Gender && model.Criteria.Gender === provider.Gender)) &&
-                    (!model.Criteria.CanTakeBehavioralChildren ||
-                    (model.Criteria.CanTakeBehavioralChildren && dataService.getCanTakeBehavioralChildrenById(model.Criteria.CanTakeBehavioralChildren) === provider.CanTakeChildrenWithBehavioralProblems))) {
+                        (!model.Criteria.Gender || (model.Criteria.Gender && model.Criteria.Gender === provider.Gender)) &&
+                        (!model.Criteria.CanTakeBehavioralChildren ||
+                        (model.Criteria.CanTakeBehavioralChildren && dataService.getCanTakeBehavioralChildrenById(model.Criteria.CanTakeBehavioralChildren) === provider.CanTakeChildrenWithBehavioralProblems))) {
                         model.filteredProviders.push(provider);
                     }
                 });
             } else {
                 model.filteredProviders = tempProviders;
-            };
+            }
+            ;
             model.sort();
 
             if (model.filteredProviders.length > 0)
                 $(".result-pagination").show();
-               $("#lblSelect").show();
-                model.currentPage = 0;
-                model.listsPerPage = model.ItemsPerPageList[0];
-                model.providersCount = model.filteredProviders.length;
+            $("#lblSelect").show();
+            model.currentPage = 0;
+            model.listsPerPage = model.ItemsPerPageList[0];
+            model.providersCount = model.filteredProviders.length;
             if ($("#selectPerPage option:selected").text()) {
                 model.currentPage = 0;
                 model.pages = Math.ceil(model.providersCount / $("#selectPerPage option:selected").text());
@@ -231,24 +241,25 @@
             model.sortData.up = !model.sortData.up;
             model.filteredProviders = _.reverse(model.filteredProviders);
         };
-        
-        $scope.$watch(function() { return angular.element("#providerSearchButton").is(':visible') }, function() {
+
+        $scope.$watch(function () {
+            return angular.element("#providerSearchButton").is(':visible')
+        }, function () {
             var criteria = queueService.getMsg('homeSearchCriteria');
-            if (!criteria)
-            {
+            if (!criteria) {
                 model.search();
                 return;
             }
-            
+
             var criteriaFromHomePage = {
-                ProviderName : criteria.providerName,
-                ProviderType : criteria.providerType,
-                City : criteria.city,
-                County : criteria.county,
-                Rate : criteria.rate,
+                ProviderName: criteria.providerName,
+                ProviderType: criteria.providerType,
+                City: criteria.city,
+                County: criteria.county,
+                Rate: criteria.rate,
                 address: criteria.zipCode,
-                distince : criteria.radius,
-                providerName : criteria.ProviderName
+                distince: criteria.radius,
+                providerName: criteria.ProviderName
             };
             model.Criteria.ProviderType = criteria.providerType;
             model.Criteria.ProviderName = criteria.providerName;
@@ -260,13 +271,13 @@
 
             queueService.setMsg('homeSearchCriteria', null);
             model.search();
-            
+
         });
     };
 
     module.component("providerSearch", {
         templateUrl: "Areas/providers/search/provider-search.html",
         controllerAs: "model",
-        controller: ["$scope", "$scope", "providerService","dataService", 'queueService', 'googleMapService',controller]
+        controller: ["$scope", "$scope", "providerService", "dataService", 'queueService', 'googleMapService', controller]
     });
 }())
