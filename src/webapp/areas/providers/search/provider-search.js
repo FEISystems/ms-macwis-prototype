c9be6b38e4f52a891e1dc50f60d7ab2bfdeb6de7
christianhfei <Christian.Heyd@fei.local>
2016-10-10 11:07:02 -0400

    Change
    Changed labels for search results to match search criteria for
    consistency. Changed all mentions of 'child care' to 'childcare.'
    
    Currently working to fix layout of search by distance down to mobile
    sizes, as the padding is a little weird.
    
    Related to issues #63, #124

--- src/webapp/areas/providers/search/provider-search.js
+++ src/webapp/areas/providers/search/provider-search.js
@@ -154,7 +154,7 @@
                     "<p class='ng-binding'><strong>USDA Food Program: </strong>  " + "N/A" + "</p></div>" +
                     "<div class='col-xs-4'><p class='ng-binding'><strong>Phone#: </strong>  " + templateModel.PhoneNumber + "</p><p class='ng-binding'> <strong>City: </strong>" + templateModel.City + " </p><p class='ng-binding'><strong>Quality Star Rating: </strong>" + templateModel.QualityRating +  "</p>  " +
                     "<p class='ng-binding'><strong>License Type: </strong>   " + templateModel.LicenseType + " </p>" +
-                    "<p ng-show='provider.CanTakeChildrenWithBehavioralProblems===true' class='ng-hide'><strong>Accepts subsidized child care: </strong>" + acceptsSubsidizedChild + "</p>" +
+                    "<p ng-show='provider.CanTakeChildrenWithBehavioralProblems===true' class='ng-hide'><strong>Accepts subsidized childcare: </strong>" + acceptsSubsidizedChild + "</p>" +
                     "<p ng-show='provider.CanTakeChildrenWithBehavioralProblems===false' class=''></p>" +
                     "<p ng-show='provider.CanTakeChildrenWithBehavioralProblems!==false&amp;&amp; provider.CanTakeChildrenWithBehavioralProblems!==true' class='ng-binding ng-hide'></p>  </div>" +
                     " <div class='col-xs-4'><p class='ng-binding'><strong>County: </strong>  " + templateModel.County + "</p>" +
